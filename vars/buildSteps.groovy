12530604d71968cd7700ffc34e0d1334693bdd64
Lior <lior@L01015153.local>
2020-05-17 21:42:48 +0200

    fix import

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -7,9 +7,17 @@ def call(ProjectConfiguration projectConfig, def dockerImage) {
     return { variables ->
         List<Step> stepsA = projectConfig.steps.steps
         def links = variables.collect { k, v -> "--link ${v.id}:${k}" }.join(" ")
-        stage('clone') {
+node {
+		stage (‘Build’) {
+			bat "msbuild ${C:\\Jenkins\\my_project\\workspace\\test\\my_project.sln}"
+		}
 
-        }
+stage('Selenium tests') {
+dir(automation_path) {  //changes the path to “automation_path”
+bat "mvn clean test -Dsuite=SMOKE_TEST -Denvironment=QA"
+}  
+}
+	}
             // stepsA.each { step ->
             //     stage(step.name) {
             //         def customImage = docker.image(step.image)
