9badd0407840f063e323e85c97af937e63054fab
Lior <lior@L01015153.local>
2020-05-17 21:26:22 +0200

    fix import

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -7,23 +7,13 @@ def call(ProjectConfiguration projectConfig, def dockerImage) {
     return { variables ->
         List<Step> stepsA = projectConfig.steps.steps
         def links = variables.collect { k, v -> "-it --link ${v.id}:${k}" }.join(" ")
-        pipeline {
-            stages {
-                stage('clone') {
-                    steps {
-                        sh "ls -la"
-                    }
-                }
+
+        stage("clone") {
+            step('update') {
+                sh "ls -la"
             }
         }
             // stepsA.each { step ->
-            //     stage(step.name) {
-            //                     node {
-            //     docker { image 'maven:3-alpine' }
-            // }
-            // steps {
-            //     sh 'mvn --version'
-            // }
             //         // def customImage = docker.image(step.image)
             //         // customImage.inside(links) {
             //         //     step.commands.each { command ->
