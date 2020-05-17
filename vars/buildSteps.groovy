53c9767dd049ef84e07d68c9ff48544e8640d33e
Lior <lior@L01015153.local>
2020-05-17 15:31:34 +0200

    fix import

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -7,19 +7,19 @@ def call(ProjectConfiguration projectConfig, def dockerImage) {
     return { variables ->
         List<Step> stepsA = projectConfig.steps.steps
         def links = variables.collect { k, v -> "--link ${v.id}:${k}" }.join(" ")
-        node() {
+        stage('Test on Linux') {
             agent {
                 label 'team-a'
             }
-            dockerImage.inside(links) {
-                stepsA.each { step ->
-                    stage(step.name) {
-                        step.commands.each { command ->
-                            sh command
-                        }
+        dockerImage.inside(links) {
+            stepsA.each { step ->
+                stage(step.name) {
+                    step.commands.each { command ->
+                        sh command
                     }
                 }
             }
+        }
         }
     }
 }
