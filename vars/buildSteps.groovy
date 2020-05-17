d37b796ebc4a01fb1eec0f6855fcba3f19cd36e7
Lior <lior@L01015153.local>
2020-05-17 21:31:41 +0200

    fix import

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -6,20 +6,20 @@ import com.wolox.steps.Step;
 def call(ProjectConfiguration projectConfig, def dockerImage) {
     return { variables ->
         List<Step> stepsA = projectConfig.steps.steps
-        def links = variables.collect { k, v -> "-it --link ${v.id}:${k}" }.join(" ")
-
+        def links = variables.collect { k, v -> "--link ${v.id}:${k}" }.join(" ")
         stage("clone") {
-            step('update') {
-
+            step("step1") {
+                sh "ls -la"
             }
         }
             // stepsA.each { step ->
-            //         // def customImage = docker.image(step.image)
-            //         // customImage.inside(links) {
-            //         //     step.commands.each { command ->
-            //         //         sh command
-            //         //     }
-            //         // }
+            //     stage(step.name) {
+            //         def customImage = docker.image(step.image)
+            //         customImage.inside(links) {
+            //             step.commands.each { command ->
+            //                 sh command
+            //             }
+            //         }
             //     }
             // }
     }
