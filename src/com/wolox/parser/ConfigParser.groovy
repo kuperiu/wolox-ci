5e06a47de5ff5046aa8fd6297c58de79d33a3749
Lior <lior@L01015153.local>
2020-05-18 14:16:42 +0200

    fix import

--- src/com/wolox/parser/ConfigParser.groovy
+++ src/com/wolox/parser/ConfigParser.groovy
@@ -72,7 +72,7 @@ class ConfigParser {
     }
 
     static def parseSecrets(def yamlSecrets) {
-        List<Step> secrets = yamlSecrets.collect { k, v ->
+        List<Secret> secrets = yamlSecrets.collect { k, v ->
             Secret secret = new Step(name: k, service: v.service, path: v.path, key: v.key) 
             return secret
         }
