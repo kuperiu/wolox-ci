776c379e3cbb9ea832bb37a8c78eaff420e128a4
Lior <lior@L01015153.local>
2020-05-18 12:24:31 +0200

    fix import

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -4,8 +4,11 @@ import com.wolox.*;
 import com.wolox.steps.Step;
 
 def vault() { 
-    script {  
-        return sh(script : 'uname -m', returnStdout: true).trim()
+    withEnv(["VAULT_ADDR=https://this.vault.dazn-dev.com", "VAULT_NAMESPACE=test-lior2"]) {
+        script {  
+            sh(script: 'vault login -method=aws role=test-lior2')
+            return sh(script : 'vault kv get -format=json kv/dev | jq .data.data.env', returnStdout: true).trim()
+        }
     }
 }
 
@@ -19,7 +22,7 @@ def call(ProjectConfiguration projectConfig, def dockerImage) {
             stepsA.each { step ->
                 stage(step.name) {
                    // def customImage = docker.image(step.image)
-                    docker.image("amazon/aws-cli").inside("--entrypoint=''")  {
+                    docker.image(step.image).inside("--entrypoint=''")  {
                         step.commands.each { command ->
                             echo "${env.SECRET}"
                             sh command
