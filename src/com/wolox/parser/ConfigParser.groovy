07a8be913359db973329d97bdcb36eb5c2bc7569
Lior <lior@L01015153.local>
2020-05-18 20:06:59 +0200

    d

--- src/com/wolox/parser/ConfigParser.groovy
+++ src/com/wolox/parser/ConfigParser.groovy
@@ -82,14 +82,13 @@ class ConfigParser {
     }
 
     static def parseStages(def yamlStages) {
-        print yamlStages.dump()
         List<Stage> stages = yamlStages.collect { k, v ->
             Stage stage = new Stage(name: k) 
             // a stage can have one or more commands to execute
-            // v.steps.each {
-            //     steps = parseSteps(it)
-            //     stage.steps.add(steps);
-            // }
+            v.steps.each {
+                steps = parseSteps(it)
+                stage.steps.add(steps);
+            }
             return stage
         }
 
