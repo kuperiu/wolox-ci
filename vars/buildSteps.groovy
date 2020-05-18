34263660e810f551361ff28e364f717bccc3818c
Lior <lior@L01015153.local>
2020-05-18 11:56:26 +0200

    fix import

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -3,22 +3,31 @@ import org.jenkinsci.plugins.workflow.libs.Library
 import com.wolox.*;
 import com.wolox.steps.Step;
 
+def vault() { 
+    script {  
+        return sh(script : 'uname -m', returnStdout: true).trim()
+    }
+}
+
 def call(ProjectConfiguration projectConfig, def dockerImage) {
     return { variables ->
         List<Step> stepsA = projectConfig.steps.steps
         //def links = variables.collect { k, v -> "--entrypoint="" --link ${v.id}:${k}" }.join(" ")
         def links = '--entrypoint=""'
+        def secret = vault()
+        withEnv(["SECRET=${secret}]") {
             stepsA.each { step ->
                 stage(step.name) {
                    // def customImage = docker.image(step.image)
                     docker.image("amazon/aws-cli").inside("--entrypoint=''")  {
                         step.commands.each { command ->
-                            echo command
+                            echo "${env.SECRET}"
                             sh command
                         }
                     }
                 }
             }
+        }
     }
 }
 
