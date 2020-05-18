69f452795b6c8a64e3b17d1b05d2de4be679e01d
Lior <lior@L01015153.local>
2020-05-18 19:59:01 +0200

    d

--- src/com/wolox/ProjectConfiguration.groovy
+++ src/com/wolox/ProjectConfiguration.groovy
@@ -3,7 +3,6 @@ package com.wolox;
 import com.wolox.docker.DockerConfiguration;
 import com.wolox.steps.Steps;
 import com.wolox.secrets.Secrets;
-import com.wolox.steps.Stages;
 
 class ProjectConfiguration {
     def environment;
