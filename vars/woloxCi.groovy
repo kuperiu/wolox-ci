57a0ddddc8fee04a41e4cd3046df46d7ed11f4a6
Lior <lior@L01015153.local>
2020-05-18 20:38:43 +0200

    d

--- vars/woloxCi.groovy
+++ vars/woloxCi.groovy
@@ -12,7 +12,9 @@ def call(String yamlName) {
     ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
 
     def imageName = projectConfig.dockerConfiguration.imageName().toLowerCase();
-print projectConfig.stages.dump()
+ projectConfig.stages.each {
+    print it.dump()
+}
     // build the image specified in the configuration
    //def customImage = docker.build(imageName, "--file ${projectConfig.dockerfile} .");
     // println "ls -l".execute().text
