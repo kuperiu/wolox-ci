ec4b6a77bbfc3603d0a2eafd26d55122b2c91e3c
Lior <lior@L01015153.local>
2020-05-19 21:21:17 +0200

    d

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -94,6 +94,7 @@ def call(ProjectConfiguration projectConfig, def dockerImage) {
                     //  parallel(parallelSteps)
                      parallelSteps.each { k, v ->
                      println(k)
+                     println(v)
                        }
                     }
 
