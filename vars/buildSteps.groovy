30c637b64a4dcba06bbf6f4ae6d395b7b94af466
Lior <lior@L01015153.local>
2020-05-19 21:47:03 +0200

    d

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -83,7 +83,7 @@ node {
 
 def doDynamicParallelSteps(){
   tests = [:]
-  for (f in ["Branch_1", "Branch_2", "Branch_3"])) {
+  for (f in ["Branch_1", "Branch_2", "Branch_3"]) {
     tests["${f}"] = {
       node {
         stage("${f}") {
