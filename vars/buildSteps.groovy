47b96799577fc1ac087506f148e80429de931536
Lior <lior@L01015153.local>
2020-05-19 10:59:39 +0200

    d

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -1,8 +1,8 @@
 import org.jenkinsci.plugins.workflow.libs.Library
 @Library('wolox-ci')
 import com.wolox.*;
-import com.wolox.steps.Stage;
-import com.wolox.steps.Step;
+//import com.wolox.steps.Stage;
+import com.wolox.steps.*;
 import com.wolox.secrets.Secret;
 
 def vault(service, path, key) { 
