a4a45ae12bd96f69906ea9ad6e85c89824aefce5
Lior <lior@L01015153.local>
2020-05-19 13:16:34 +0200

    d

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -108,7 +108,7 @@ def prepareBuildStages(stages, steps) {
     stages.each { stage ->
         stage.steps.each { stepName ->
             println(stepName)
-            println(steps.build)
+            println(steps.dump())
         }
     }
 
