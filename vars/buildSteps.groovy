7cd1a2ba5a2b3bf588dee9b253a526a664758d74
Lior <lior@L01015153.local>
2020-05-20 11:32:42 +0200

    d

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -92,6 +92,7 @@ def call(ProjectConfiguration projectConfig, def dockerImage) {
                                             }
                                             if (stepsA[index].name == "test") {
                                                     junit 'report.xml'
+                                                    echo currentBuild.result
                                                     if (currentBuild.result == 'UNSTABLE') {
                                                         currentBuild.result = 'FAILURE'
                                                     }
